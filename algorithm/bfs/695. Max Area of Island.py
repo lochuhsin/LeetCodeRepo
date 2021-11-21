22f5e3d69e56ce0e423ec89d0be998825c51fd9f
albert <lochuhsin@gmail.com>
2021-11-21 10:10:58 +0800

    add solotion to max_island

--- algorithm/bfs/695. Max Area of Island.py
+++ algorithm/bfs/695. Max Area of Island.py
@@ -1,5 +1,6 @@
 '''
 just run bfs or dfs, sum the area
+however. mine solution reaches TLE
 '''
 from collections import deque
 
@@ -29,3 +30,34 @@ class Solution:
                 if grid[r][c] == 1:
                     max_area = max(max_area, bfs((r, c), grid))
         return max_area
+
+'''
+This solution uses seen dic, can't figure out why is faster. (I change value)
+'''
+
+from collections import deque
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid):
+        seen = set()
+        ans = 0
+        for r0, row in enumerate(grid):
+            for c0, val in enumerate(row):
+                if val and (r0, c0) not in seen:
+                    shape = 0
+                    stack = [(r0, c0)]
+                    seen.add((r0, c0))
+                    while stack:
+                        r, c = stack.pop()
+                        shape += 1
+                        for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
+                            if (0 <= nr < len(grid) and 0 <= nc < len(grid[0])
+                                    and grid[nr][nc] and (nr, nc) not in seen):
+                                stack.append((nr, nc))
+                                seen.add((nr, nc))
+                    ans = max(ans, shape)
+        return ans
+
+
+
