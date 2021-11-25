1ea5ce17122f344c04979adf5bbe2684aeb4a72d
Albert Lo <AlbertLo@gbd730.local>
2021-11-25 09:34:58 +0800

    implement tree right rotation and tree left rotation

--- algorithm/playground/tree.py
+++ algorithm/playground/tree.py
@@ -4,3 +4,20 @@ class node(object):
         self.val = val
         self.left = left
         self.right = right
+
+
+# tree right rotation
+def right_rotation(node: 'node') -> 'node':
+
+    pointer = node.left
+    node.left = pointer.right
+    pointer.right = node 
+    return pointer
+
+# tree left rotation
+def left_rotation(node: 'node') -> 'node':
+
+    pointer = node.right
+    node.right = pointer.left
+    pointer.left = node
+    return pointer
